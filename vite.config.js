d2276b420f31622165df9ca50efcba170bc3b9f2
iimrancr7 <iimrancr7@gmail.com>
2025-04-23 16:32:55 +0600

    page linking

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        services: path.resolve(__dirname, 'services.html'),
+        project: path.resolve(__dirname, 'project.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+        blogs: path.resolve(__dirname, 'blogs.html'),
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
